0626c89bfb17652b2b86cd2920fb1b4dd0ce5715
ChanKi Kim <97859215+CKtrace@users.noreply.github.com>
2022-07-25 22:27:07 +0900

    Matching Regex

--- How to deal with Data and Machine Learning/Matching_Regex.py
+++ How to deal with Data and Machine Learning/Matching_Regex.py
@@ -1,7 +1,6 @@
 import sys, re
 
 regex = sys.argv[1] #sys.argv[0] -> Output = Scipt Name ex) ".\Sample_Data.txt" 
-
 for line in sys.stdin:
     if re.search(regex, line):
         sys.stdout.write(line)
